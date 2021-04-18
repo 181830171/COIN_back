0bf96bdc5b130858cfa4c764de4ab805d7b83753
181830171 <181830171@smail.nju.edu.cn>
2021-04-18 17:59:43 +0800

    历史搜索记录去重

--- src/main/java/com/example/neo4jKG/Dao/SearchHistoryRepository.java
+++ src/main/java/com/example/neo4jKG/Dao/SearchHistoryRepository.java
@@ -1,10 +1,17 @@
 package com.example.neo4jKG.Dao;
 
+import com.example.neo4jKG.Entity.NeoEntity;
 import com.example.neo4jKG.Entity.Relation;
 import com.example.neo4jKG.Entity.SearchHistory;
 import org.springframework.data.neo4j.repository.Neo4jRepository;
+import org.springframework.data.neo4j.repository.query.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface SearchHistoryRepository extends Neo4jRepository<SearchHistory, Long> {
+    @Query("MATCH (n) WHERE n.history = :#{#history} RETURN n")
+    List<SearchHistory> findByHistory(@Param(value = "history") String history);
 }
